efc026e090a99ecd428a4d5dfa446a34149854ce
TriGerZ <franck.mazzolo@gmail.com>
2015-03-29 20:36:40 +0200

    Adding FOSUserBundle in the kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -3,12 +3,9 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-class AppKernel extends Kernel
-{
-    
+class AppKernel extends Kernel {
 
-    public function registerBundles()
-    {
+    public function registerBundles() {
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
@@ -20,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Kartographerz\CartographyBundle\KartographerzCartographyBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
@@ -32,15 +30,13 @@ class AppKernel extends Kernel
         return $bundles;
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    public function registerContainerConfiguration(LoaderInterface $loader) {
+        $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
     }
-    
-        public function init()
-    {
-        date_default_timezone_set( 'Europe/Paris' );
+
+    public function init() {
+        date_default_timezone_set('Europe/Paris');
         parent::init();
     }
-    
+
 }
