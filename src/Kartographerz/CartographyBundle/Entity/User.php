0a707cde4cb8056edbce299fe56086fe2b18b3b0
Franck Mazzolo <franck.mazzolo@gmail.com>
2015-04-21 23:11:02 +0200

    Overwriting default user to match our own

--- src/Kartographerz/CartographyBundle/Entity/User.php
+++ src/Kartographerz/CartographyBundle/Entity/User.php
@@ -4,6 +4,7 @@ namespace Kartographerz\CartographyBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * User
@@ -19,6 +20,27 @@ class User extends BaseUser {
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname", type="string", length=255, nullable=false)
+     */
+    private $firstname;
+
+    /**
+     * @var \Enterprise
+     *
+     * @ORM\OneToOne(targetEntity="Kartographerz\CartographyBundle\Entity\Enterprise",  cascade={"persist"}))
+     */
+    private $enterprise;
 
     function __construct() {
         parent::__construct();
