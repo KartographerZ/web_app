741b241090ea5acebb90385e57c64bae9797ab14
Yacouba DJIBO <y.djibo@homail.com>
2015-05-17 13:01:45 +0200

    LoadGroupData a été créé pour importer les données des tables version, version_element, element et cartography. Le but est d'avoir des données déjà présentes pour tester la fonction "précocher les éléments de la cartographie"

---
+++ src/Kartographerz/CartographyBundle/DataFixtures/ORM/LoadGroupData.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of LoadUserGroup
+ *
+ * @author franckmazzolo
+ */
+class LoadGroupData extends \Doctrine\Common\DataFixtures\AbstractFixture implements \Doctrine\Common\DataFixtures\FixtureInterface, \Doctrine\Common\DataFixtures\OrderedFixtureInterface {
+
+    public function load(\Doctrine\Common\Persistence\ObjectManager $manager) {
+        $adminsGroup = new Kartographerz\UserBundle\Entity\Group('administration');
+        $adminsGroup->addRole('ROLE_ADMIN');
+        $manager->persist($adminsGroup);
+
+        $manager->flush();
+
+        $this->addReference('admin-group', $adminsGroup);
+    }
+
+    public function getOrder() {
+        return 1;
+    }
+
+//put your code here
+}
